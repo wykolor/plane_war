9890289262c7ef7052daaed8bf4702d099de2a08
wykolor <kolor1121460147@163.com>
2021-08-26 11:26:27 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+const path = require('path');
+module.exports = {
+  entry: path.resolve(__dirname, "./main.js"),
+  output: {
+    filename: "build.js",
+    path: path.resolve(__dirname, "./dist")
+  },
+}
